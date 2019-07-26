52b2e50a2b17f9fd9e99ec5c6535251bfdcfedbe
Diego Mais <diegomais@live.com>
2019-07-26 06:34:56 -0300

    Add global styles
    - This component added to component tree inject global styles into the application.
    
    On branch master
    Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/styles/global.js
    	modified:   yarn.lock

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -6,10 +6,13 @@ import './config/Reactotron';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyles from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyles />
     </Router>
   );
 }
