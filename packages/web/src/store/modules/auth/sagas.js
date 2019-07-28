b7909c981f5bd56c7200ec6566298301d5c73d24
Diego Mais <diegomais@live.com>
2019-07-27 23:31:18 -0300

    Add loading message at auth
    - This improvement provides a message to user when waiting for response from backend.
    
    On branch master
    Changes to be committed:
    	modified:   src/pages/SignIn/index.js
    	modified:   src/store/modules/auth/reducer.js
    	modified:   src/store/modules/auth/sagas.js

--- packages/web/src/store/modules/auth/sagas.js
+++ packages/web/src/store/modules/auth/sagas.js
@@ -3,23 +3,27 @@ import { call, put, all, takeLatest } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
   const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', { email, password });
+  try {
+    const response = yield call(api.post, 'sessions', { email, password });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('User is not a provider.');
-    return;
+    if (!user.provider) {
+      console.tron.error('User is not a provider.');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
