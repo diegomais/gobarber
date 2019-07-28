e3b49e5b2f7b0e0c450813c2c2001de2061c46f0
Diego Mais <diegomais@live.com>
2019-07-27 21:15:11 -0300

    Add state container and set up Reactotron
    - Redux is a predictable state container.
    - redux-saga is an alternative side effect model for Redux.
    
    On branch master
    Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	modified:   src/config/Reactotron.js
    	new file:   src/store/createStore.js
    	new file:   src/store/index.js
    	new file:   src/store/modules/auth/actions.js
    	new file:   src/store/modules/auth/reducer.js
    	new file:   src/store/modules/auth/sagas.js
    	new file:   src/store/modules/rootReducer.js
    	new file:   src/store/modules/rootSaga.js
    	modified:   yarn.lock

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/Reactotron';
+import store from './store';
 
 import Routes from './routes';
 import history from './services/history';
@@ -10,10 +12,12 @@ import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </Provider>
   );
 }
 
