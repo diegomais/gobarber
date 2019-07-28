ba4d2c1817c5173ccd1c4a7def31e2576c2c93d3
Diego Mais <diegomais@live.com>
2019-07-27 22:45:30 -0300

    Add auth module to state container
    On branch master
    Changes to be committed:
    	modified:   package.json
    	modified:   src/pages/SignIn/index.js
    	modified:   src/routes/Route.js
    	new file:   src/services/api.js
    	modified:   src/store/modules/auth/actions.js
    	modified:   src/store/modules/auth/reducer.js
    	modified:   src/store/modules/auth/sagas.js
    	modified:   yarn.lock

--- packages/web/src/store/modules/auth/reducer.js
+++ packages/web/src/store/modules/auth/reducer.js
@@ -1,9 +1,18 @@
+import produce from 'immer';
+
 const initialState = {
-  token: '123',
+  token: null,
+  signed: false,
+  loading: false,
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }
--- packages/web/src/store/modules/auth/sagas.js
+++ packages/web/src/store/modules/auth/sagas.js
@@ -1,3 +1,25 @@
-import { all } from 'redux-saga/effects';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', { email, password });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('User is not a provider.');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
