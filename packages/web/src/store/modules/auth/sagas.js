82550e94820509153532bbcf2cb6657e8f4fb479
Diego Mais <diegomais@live.com>
2019-07-27 23:55:25 -0300

    Add sign up request action
    - This action provides user to register a new user.
    
    On branch master
    Changes to be committed:
    	modified:   src/pages/SignUp/index.js
    	modified:   src/store/modules/auth/actions.js
    	modified:   src/store/modules/auth/sagas.js

--- packages/web/src/store/modules/auth/sagas.js
+++ packages/web/src/store/modules/auth/sagas.js
@@ -28,4 +28,21 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', { name, email, password, provider: true });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Registration failed, check your data.');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
