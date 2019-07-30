63b8944283a7ec169e7bf0dd37f9f2008d9ced2d
Diego Mais <diegomais@live.com>
2019-07-30 07:36:41 -0300

    Update header to get profile from state container
    - This improvement provides header to get user profile from state container.
    
    On branch master
    Changes to be committed:
    	modified:   src/components/Header/index.js

--- packages/web/src/components/Header/index.js
+++ packages/web/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -7,6 +8,8 @@ import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -20,12 +23,16 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Diego Mais</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">My profile</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Diego Mais"
+              src={
+                profile.avatar
+                  ? profile.avatar.url
+                  : 'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
