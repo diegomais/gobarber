fee95fd10b467c982a5ae4ce39b4b7a21c818b6d
Diego Mais <diegomais@live.com>
2019-07-31 07:59:59 -0300

    Add navigation between days on dashboard
    - This improvement provides user to navigate between days on dashboard.
    
    On branch master
    Changes to be committed:
    	modified:   src/pages/Dashboard/index.js

--- packages/web/src/pages/Dashboard/index.js
+++ packages/web/src/pages/Dashboard/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { format, subDays, addDays } from 'date-fns';
 
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(() => format(date, 'MMMM do'), [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>31 May</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
