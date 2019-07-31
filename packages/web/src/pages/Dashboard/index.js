c449a7d54f7f201f55e96d6e27698ac84ebc1567
Diego Mais <diegomais@live.com>
2019-07-31 07:14:20 -0300

    Add dashboard UI
    - This improvement provides the user interface for dashboard page.
    
    On branch master
    Changes to be committed:
    	modified:   src/pages/Dashboard/index.js
    	new file:   src/pages/Dashboard/styles.js

--- packages/web/src/pages/Dashboard/index.js
+++ packages/web/src/pages/Dashboard/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 May</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Michael Doe</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Available</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Mary Doe</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>John Doe</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
