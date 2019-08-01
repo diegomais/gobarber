4842108ec2c359e5bb7f757ff8f1a349ce2b384c
Diego Mais <diegomais@live.com>
2019-08-01 06:39:52 -0300

    Add get appointments from database
    - This improvement provides user to get appointments from database and list then by date.
    
    On branch master
    Changes to be committed:
    	modified:   package.json
    	modified:   src/pages/Dashboard/index.js
    	modified:   yarn.lock

--- packages/web/src/pages/Dashboard/index.js
+++ packages/web/src/pages/Dashboard/index.js
@@ -1,15 +1,55 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-import { format, subDays, addDays } from 'date-fns';
+import {
+  format,
+  subDays,
+  addDays,
+  setSeconds,
+  setMinutes,
+  setHours,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+// TODO: Set range on backend.
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+  const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
 
   const dateFormatted = useMemo(() => format(date, 'MMMM do'), [date]);
+
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', { params: { date } });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkHour = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareHour = utcToZonedTime(checkHour, timezone);
+
+        return {
+          time: `${hour}:00`,
+          past: isBefore(compareHour, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareHour)
+          ),
+        };
+      });
+
+      setSchedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
 
   function handlePrevDay() {
     setDate(subDays(date, 1));
@@ -31,22 +71,14 @@ export default function Dashboard() {
         </button>
       </header>
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Michael Doe</span>
-        </Time>
-        <Time available>
-          <strong>09:00</strong>
-          <span>Available</span>
-        </Time>
-        <Time>
-          <strong>10:00</strong>
-          <span>Mary Doe</span>
-        </Time>
-        <Time>
-          <strong>08:00</strong>
-          <span>John Doe</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Available'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
